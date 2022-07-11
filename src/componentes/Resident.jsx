f5cf7c78c1ad171e56a5f1bf1e3741f40c1f0c14
fdlaurentis <fdlaurentis@gmail.com>
2022-07-11 09:00:29 -0500

    iconos status

--- src/componentes/Resident.jsx
+++ src/componentes/Resident.jsx
@@ -7,14 +7,34 @@ const Resident = ({ resident }) => {
         axios.get(resident)
             .then(res => setCharacter(res.data))
     }, [])
-    
+
+    const status=()=>{
+        if (character.status=='Dead'){
+            return(
+                <i className="dead fa-solid fa-skull-crossbones"></i>
+            )
+        } else if(character.status=='Alive'){
+            return(
+                <i className="life fa-solid fa-heart"></i>
+            )
+        }else{
+            return(
+                <i class="unknown fa-solid fa-circle-question"></i>
+            )
+        }
+    }
+                
+                
+      
+   
+
     return (
         <div className='card'>
             <img src={character.image} alt="" />
             <div className="data">
-                <h2>Name: <span>{character.name}</span></h2> 
-                <h2>Type: <span>{(character.type==""?"Unknown":character.type)}</span></h2> 
-                <h2>Status: <span>{(character.status=="Dead"?<><i className="dead fa-solid fa-skull-crossbones"></i> { character.status}</>:character.status)}</span></h2>
+                <h2>Name: <span>{character.name}</span></h2>
+                <h2>Type: <span>{(character.type == "" ? "Unknown" : character.type)}</span></h2>
+                <h2>Status: <span>{status()} {character.status}</span></h2>
                 <h2>Origin: <span>{character.origin?.name}</span></h2>
                 <h2>Episode: <span>{character.episode?.length}</span></h2>
 
